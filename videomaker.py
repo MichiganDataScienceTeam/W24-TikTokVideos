bd4e5ce9f3a7a0990c25a5648d0bfe54f1d130e0
TobyWan23 <tqbuckfire@hotmail.com>
2024-02-18 14:31:12 -0500

    Download videos, trim length, put in video folder
    Co-authored-by: parkerdamaska <parkerdamaska@users.noreply.github.com>

--- videomaker.py
+++ videomaker.py
@@ -1,5 +1,8 @@
 import click
-
+import shutil
+from pytube import YouTube
+from moviepy.editor import *
+#
 
 @click.command()
 @click.option(
@@ -15,7 +18,20 @@ import click
 def main(videos: int, subreddit: str):
     """Automatically generate videos from Reddit posts."""
     print(f"Generating {videos} videos from r/{subreddit}.")
+    downloadVideos()
 
+def downloadVideos():
+    #yt = YouTube('https://www.youtube.com/watch?v=Q5KtBKk4hC0')
+    # video name - Subway Surfers - First 30 Minutes Gameplay (Vertical Video).mp4
+    yt = YouTube('https://www.youtube.com/watch?v=Q5KtBKk4hC0')
+    str = yt.streams[0].title + ".mp4"
+    yt.streams.filter(res="720p").first().download(output_path = 'videos')
+    #video = VideoFileClip("Subway Surfers - First 30 Minutes Gameplay (Vertical Video).mp4").subclip(1622, 1682) # from 27:02 to 28:02
+    video = VideoFileClip(str).subclip(1622, 1682) # from 27:02 to 28:02
+    video.write_videofile("videos/background_edited.mp4",fps=25) #was a .webm
+
+   
 
 if __name__ == "__main__":
     main()
+
