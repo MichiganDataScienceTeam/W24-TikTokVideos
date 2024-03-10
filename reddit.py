fb9cea1d51a7fad98a120b8ae0e60a0f9745ffcd
Parin Raizada <parinr@umich.edu>
2024-03-10 12:56:00 -0400

    added tokens

--- reddit.py
+++ reddit.py
@@ -1,8 +1,8 @@
 import praw 
 
 reddit = praw.Reddit(
-    client_id="my client id",
-    client_secret="my client secret",
+    client_id="mLSSuv3e-6UN_LXdXAF1SA",
+    client_secret="Mh4TAZlT_9okW95i_IPnPByMKI0zug",
     password="my password",
     user_agent="my user agent",
     username="my username",
