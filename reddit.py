356713a9cc1723419d902741cd67ddd4b550b236
Parin Raizada <parinr@umich.edu>
2024-03-10 13:10:37 -0400

    new username

--- reddit.py
+++ reddit.py
@@ -1,9 +1,14 @@
 import praw 
 
 reddit = praw.Reddit(
-    client_id="mLSSuv3e-6UN_LXdXAF1SA",
-    client_secret="Mh4TAZlT_9okW95i_IPnPByMKI0zug",
-    password="my password",
-    user_agent="my user agent",
-    username="my username",
-)
+    client_id="qO_E9x26ahBwZwt0E4C6gQ",
+    client_secret="	_DbAEWbLn8HtwkZafiU3KqW349Ae9g",
+    user_agent="r/AskReddit comment extraction by u/MDSTBot",
+    username="MDSTBot",
+    password="Winter-2024"
+    )
+
+url = "https://www.reddit.com/r/AskReddit/comments/1bb49z1/what_was_cool_in_2014_but_isnt_cool_in_2024/"
+submission = reddit.submission(url=url)
+for top_level_comment in submission.comments:
+    print(top_level_comment.body)
