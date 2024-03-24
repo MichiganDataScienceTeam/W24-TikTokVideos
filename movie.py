eddfce5d2117757091eaaee779d102e31cca9d5c
aweyhing <aweyhing@umich.edu>
2024-03-24 15:05:55 -0400

    wrote loop to do tts and merge with minecraft videos

--- movie.py
+++ movie.py
@@ -4,10 +4,11 @@ from moviepy.editor import *
 def videomaker():
 
     # need to match the timings
-    audioclip = AudioFileClip("test0.mp3")
-    videoclip = VideoFileClip("backgroundvideos/minecraftdiamonds.mp4").subclip(0,audioclip.duration)
-    new_audioclip = CompositeAudioClip([audioclip])
-    videoclip.audio = new_audioclip
-    videoclip.write_videofile("minecraft_edited.mp4")
+    for i in range(3):
+        audioclip = AudioFileClip('test' + str(i) + '.mp3')
+        videoclip = VideoFileClip("backgroundvideos/minecraftdiamonds.mp4").subclip(0,audioclip.duration)
+        new_audioclip = CompositeAudioClip([audioclip])
+        videoclip.audio = new_audioclip
+        videoclip.write_videofile("minecraft_edited" + str(i) + ".mp4")
 
 videomaker()
--- scrape_tts_post.py
+++ scrape_tts_post.py
@@ -63,15 +63,10 @@ def read_post():
     top_posts = pd.DataFrame(posts_dict)
 
     # read the first _ posts from top_posts and save into separate mp3's
-    for i in range(1):
-        # print(top_posts["Post Text"][i])
+    for i in range(3):
         str = clean(top_posts["Post Text"][i], no_emoji = True)
-        # str = re.sub(r'[^A-Za-z0-9 \n]+', '', top_posts["Post Text"][i])
-        # str = str.encode("utf-8").decode("ascii", "ignore")
-        # print(posts_dict["Post URL"][i])
-        # print("line")
-        print(str)
-        # read(str, i)
+        # print(str)
+        read(str, i)
 
 
 read_post()
